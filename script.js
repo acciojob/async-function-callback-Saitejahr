7fb2dae38f489eb57a0591511c74d6ec1b0e13ac
SAI TEJA H R <98202897+Saitejahr@users.noreply.github.com>
2023-03-29 19:51:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
-
-//your JS code here. If required.
+async function fetchData(callback) {
+    const response = await fetch(url);
+    const data = await response.json();
+    callback(data);
+  }
+  
+  function displayMessage(data) {
+      document.getElementById("output").innerText = data.title;
+  }
+  // add onclick="fetchData(displayMessage)" on button in html.
